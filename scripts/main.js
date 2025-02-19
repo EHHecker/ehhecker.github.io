8d34083cc63fde487a5f2923c0f6677fda1d2814
EHHecker <ehhecker@coastal.edu>
2025-02-19 13:16:09 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,60 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+const myImage = document.querySelector("img");
+myImage.addEventListener("click", () => {
+	const mySrc = myImage.getAttribute("src");
+	if (mySrc === "images/glasses.png") {
+		myImage.setAttribute("src", "images/glassesshatter.png");
+	} else {
+		myImage.setAttribute("src", "images/glasses.png");
+	}
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+	const myName = prompt("Please enter your name.");
+	localStorage.setItem("name", myName);
+	myHeading.textContent = `Glasses are cool, ${myName}`;
+}
+if (!localStorage.getItem("name")) {
+	setUserName();
+} else {
+	const storedName = localStorage.getItem("name");
+	myHeading.textContent = `glasses are cool, $(storedName)`;
+}
+myButton.addEventListener("click", () => {
+	setUserName();
+});
+
+/*
+let myVariable;
+myVariable = "Bob";
+let myVariable = "Bob";
+myVariable;
+let myVariable = "Bob";
+myVariable = "Steve";
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+	alert("Yay, I love chocolate ice cream!");
+} else {
+	alert("Awww, but chocolate is my favorite...");
+}
+let myVariable = document.querySelector("h1");
+alert("hello!");
+function multiply(num1, num2) {
+	let result = num1 * num2;
+	return result;
+}
+multiply(4, 7);
+multiply(20, 20);
+multiply(0.5, 3);
+document.querySelector("html).addEventListener("click", function() {
+	alert("Ouch! Stop poking me!");
+});
+document.querySelector("html").addEventListener("click", () => {
+	alert("Ouch! Stop poking me!");
+});
+*/
